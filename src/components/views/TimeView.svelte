09ca30e676d2813c3d3b9c9d16413a634caa716b
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2020-12-28 11:04:19 +0000

    Fix dumb bug with timer

--- src/components/views/TimeView.svelte
+++ src/components/views/TimeView.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
     import type Controller from "../../controller/Controller";
 
-
     export let controller: Controller;
-    let mins = 0, secs = 0, cents = 0;
+    let mins = 0,
+        secs = 0,
+        decimal = 0;
 
     function updateTime() {
-        let diff = $controller.startTimestamp ? ($controller.finishTimestamp ?? Date.now()) - $controller.startTimestamp : 0;
+        let diff = $controller.startTimestamp
+            ? ($controller.finishTimestamp ?? Date.now()) -
+              $controller.startTimestamp
+            : 0;
 
         mins = Math.floor(diff / 1000 / 60);
-        secs = Math.floor(diff / 1000) % 1000;
-        cents = Math.floor(diff / 10) % 100;
+        secs = Math.floor(diff / 1000) % 60;
+        decimal = Math.floor(diff / 100) % 10;
     }
 
-    setInterval(updateTime, 10)
+    setInterval(updateTime, 10);
 </script>
-
-
-<div>{mins.toString().padStart(2, "0")}:{secs.toString().padStart(2, "0")}.{cents.toString().padStart(2, "0")}</div>
 
 <style>
     div {
@@ -26,3 +27,9 @@
         text-align: center;
     }
 </style>
+
+<div>
+    {mins
+        .toString()
+        .padStart(2, '0')}:{secs.toString().padStart(2, '0')}.{decimal}
+</div>
